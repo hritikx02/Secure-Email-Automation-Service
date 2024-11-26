47bab11aa8a471dafe51923f620758e5247a26d5
Hritik Chaudhary <hritikchoudhary943@gmail.com>
2024-11-26 19:00:06 +0530

    First Commit

---
+++ config/emailConfig.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports = {
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    },
+    secure: true
+};
